87e4b38c0a1341b2327125218fe4de44a61ac1a5
Finn Shields <fshields@c2r6p1.hive.fi>
2024-06-12 08:32:33 +0300

    updated simple server

--- simpleServer.cpp
+++ simpleServer.cpp
@@ -6,7 +6,7 @@
 /*   By: fshields <fshields@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:16:17 by fshields          #+#    #+#             */
-/*   Updated: 2024/06/11 14:16:07 by fshields         ###   ########.fr       */
+/*   Updated: 2024/06/12 08:18:20 by fshields         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,22 @@ int	main()
 {
 	int	listen_fd;
 	int	conn_fd;
+	int opt = 1;
 	struct sockaddr_in serv_addr;
 	char buffer[1024];
-	std::string msg = "hello";
+	std::string msg = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, World!";
 
-	listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (listen_fd == -1)
 	{
 		std::cerr << "socket error" << std::endl;
 		return (1);
 	}
+	if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+    }
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(SERVER_PORT);
@@ -50,7 +56,7 @@ int	main()
 			std::cout << "connection established" << std::endl;
 		if (send(conn_fd, &msg, 5, 0) == -1)
 			std::cerr << "send error" << std::endl;
-		if (recv(conn_fd, buffer, 1024, MSG_WAITALL) == -1)
+		if (recv(conn_fd, buffer, 1024, 0) == -1)
 			std::cerr << "recv error" << std::endl;
 		else
 			std::cout << "send message" << std::endl;
