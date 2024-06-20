85114509ae480d58725aa6fd25dbfd037cca2370
Alexandr Pimikov <apimikov@student.hive.fi>
2024-06-20 10:46:53 +0300

    touch

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,5 +1,5 @@
-#ifndef PARCECONF_HPP
-# define PARCECONF_HPP
+#ifndef CONFIG_HPP
+# define CONFIG_HPP
 
 #include <string>
 #include <iostream>
@@ -9,22 +9,11 @@
 #include <map>
 #include <algorithm> 
 
-/* here we follow the EBNF given next:
-	S -> server S'
-	S' -> server S' | eps
-	server -> 'server' '{' group_list '}'
-	group_list -> group group_list | eps
-	group -> 'group' identifier '{' key_value_list '}'
-	key_value_list -> key_value key_value_list | eps
-	key_value -> identifier value_list ';'
-	value_list -> identifier value_list | eps
-	identifier -> [non whitespace character]*  exclude ; server group
-	eps = empty set
-*/
+#include "Parser.hpp"
 
-using t_vector_str = std::vector<std::string>;
-using t_group = std::map<std::string, t_vector_str>;
-using t_server = std::map<std::string, t_group>;
+/* This class is defined to keep and manage data extracted 
+	by parsing congifig file. 
+*/
 
 class Config
 {
@@ -34,19 +23,6 @@ class Config
 		Config(Config &);
 		Config& operator=(Config&);
 		Config(std::string);
-		std::string readFile(std::string filename) const;
-		enum class tok {server, group, word, semicol, //newline, 
-			open, close, eof};
-		void skipComment();
-		tok peek();
-		bool isAnyWord(tok token);
-		tok getToken();
-		std::vector<t_server>& parseFile();
-		t_server parseServer();
-		t_group parseGroupSetting();
-		t_vector_str parseWordList();
-		std::string parseWord();
-		std::string leftoverString();
 		size_t	size();
 		std::vector<t_server>& get();
 		t_server get(size_t server);
@@ -59,22 +35,8 @@ class Config
 				const char* what() const noexcept override;
 		};*/
 	private:
-		std::string _filecontent;
-		tok		_token;
-		size_t	_endcontent;
-		size_t	_size;
-		size_t	_position;
-		size_t	_tok_begin;	
-		size_t	_tok_end;
-		//int		_open_par;
-		std::vector<t_server> _data;
+		const std::vector<t_server> _data;
 };
-
-std::ostream& operator<<(std::ostream& os, t_vector_str& vs);
-std::ostream& operator<<(std::ostream& os, t_group& l);
-std::ostream& operator<<(std::ostream& os, t_server& l);
-std::ostream& operator<<(std::ostream& os, std::vector<t_server>& file);
-
 
 #endif
 
