fa935491f98d0ff1450287851e56ed15059ec15b
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-06-26 13:50:15 +0300

    added delete page, delete button next to files

--- src/Response.cpp
+++ src/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: bsyvasal <bsyvasal@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 13:05:15 by bsyvasal          #+#    #+#             */
-/*   Updated: 2024/06/14 17:56:19 by bsyvasal         ###   ########.fr       */
+/*   Updated: 2024/06/26 13:46:45 by bsyvasal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,29 @@ std::string Response::get()
 	std::string dir = _req.get("target");
 	
 	std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nYour request: " + method + " " + dir;
+	std::cout << response << std::endl;
 	response.append(" srv: " + _srv.get_name());
 	if (dir == "/")
-		response = load_index();
+		return load_index();
+	if (dir == "/delete")
+		return load_directory_listing();
 	return (response);
 }
 
 std::string Response::post()
 {
+	saveFile();
 	return ("HTTP/1.1 204 No Content");
 }
 
 std::string Response::deleteResp()
 {
+	std::string target = _req.get("target");
+	std::string dir = "uploads";
+
+	replacePercent20withSpace(target);
+	target = dir + target;
+	deleteFile(target);
 	return ("HTTP/1.1 204 No Content");
 }
 
@@ -61,4 +71,82 @@ std::string Response::load_index()
 	file.close();
 
 	return ("HTTP/1.1 200 OK\nContent-Type: text/html\n\n" + buffer.str());
+}
+std::string Response::load_directory_listing()
+{
+    DIR* dir;
+    struct dirent* ent;
+    std::string directoryPath = "uploads";
+    std::stringstream buffer;
+
+    if ((dir = opendir(directoryPath.c_str())) != NULL) {
+        buffer << "<html><head><title>Directory Listing</title></head><body>";
+        buffer << "<h2>Directory Listing of /uploads</h2>";
+        buffer << "<ul>";
+
+        // List all the files and directories within directory
+        while ((ent = readdir(dir)) != NULL) 
+		{
+			if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0)
+			{
+				buffer << "<li>" << ent->d_name
+				<< "<button id=\"" << ent->d_name << "\">Delete</button>" 
+				<< "<script>document.getElementById(\"" << ent->d_name << "\").addEventListener(\"click\", function() {var delReq = new XMLHttpRequest();"
+				<< "delReq.open(\"DELETE\", \"/" << ent->d_name << "\", false);"
+				<< "delReq.send(null);})</script></li>";
+			}
+        }
+        buffer << "</ul>";
+        buffer << "</body></html>";
+
+        closedir(dir);
+    } else {
+		std::cout << "couldnt open dir" << std::endl;
+        return ("HTTP/1.1 500 Internal Server Error\nContent-Type: text/plain\n\nError: Could not open directory");
+    }
+
+    return ("HTTP/1.1 200 OK\nContent-Type: text/html\n\n" + buffer.str());
+}
+
+void Response::saveFile()
+{
+    if (_req.get("Content-Type").compare(0, 19, "multipart/form-data"))
+        return ;
+    std::string boundary = _req.get("Content-Type").substr(31);
+    std::string body = _req.get("body");
+    std::string fileName = "";
+    std::string::iterator it = body.begin();
+    it += body.find("filename") + 10;
+    while (*it != '\"')
+        fileName.append(1, *(it++));
+    if (fileName.empty())
+        return ;
+    std::string directory = "uploads/";
+    mkdir(directory.c_str(), 0777);
+    fileName = directory + fileName;
+    size_t start = body.find("\r\n\r\n") + 4;
+    size_t len = body.find_last_of(boundary) - boundary.length() - 6 - start;
+    std::string fileContent = body.substr(start, len);
+    std::ofstream newFile(fileName);
+    newFile << fileContent;
+    newFile.close();
+    _srv.setFileName(fileName);
+}
+
+void Response::deleteFile(std::string &file)
+{
+    if (std::remove(file.c_str()) < 0)
+        perror("remove");
+	else
+		std::cout << "deleted " << file << std::endl;
+}
+
+void Response::replacePercent20withSpace(std::string &str)
+{
+	size_t pos = str.find("%20");
+	while (pos != std::string::npos)
+	{
+		str.replace(pos, 3, " ");
+		pos = str.find("%20", pos + 1);
+	}
 }
