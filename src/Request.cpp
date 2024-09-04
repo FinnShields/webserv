b99509a45e0de544a17f91dfd221e944ae084bdc
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-09-04 15:12:24 +0300

    Request::read() return 3 when CGI and body not fully read

--- src/Request.cpp
+++ src/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: bsyvasal <bsyvasal@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 08:43:48 by fshields          #+#    #+#             */
-/*   Updated: 2024/08/28 10:47:05 by bsyvasal         ###   ########.fr       */
+/*   Updated: 2024/09/04 15:03:34 by bsyvasal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,10 +97,14 @@ int	Request::read(int _fd)
         !_headers["content-length"].empty() ? 1 : 0; 
     
     if (_status == 1)
-        return std::stol(_headers["content-length"]) > _bodyTotalSize ? 1 : 0;
-    return _chunkedReqComplete ? 0 : !_chunkedReqComplete ? 1 : 0;
+        return std::stol(_headers["content-length"]) > _bodyTotalSize ? (isCGI() ? 3 : 1) : 0;
+    return _chunkedReqComplete ? 0 : !_chunkedReqComplete ? (isCGI() ? 3 : 1) : 0;
 }
 
+int Request::isCGI()
+{
+     return (_target.size() > 9 && !_target.substr(0, 9).compare("/cgi-bin/"));
+}
 bool Request::isWholeHeader()
 {
     char *ch = strstr(_reqRaw.data(), "\r\n\r\n");
