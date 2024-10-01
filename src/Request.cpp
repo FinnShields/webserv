bbb867e730cfd95bab6087863ec4265db6af845a
Bobbie Syvasalmi <bsyvasal@student.hive.fi>
2024-10-01 13:39:14 +0300

    Updated cgi-bin to not be always cgi

--- src/Request.cpp
+++ src/Request.cpp
@@ -310,10 +310,6 @@ bool Request::IsBodyIncomplete()
 
 int Request::setCGIflag()
 {
-    if ((_target.size() > 9 && !_target.substr(0, 9).compare("/cgi-bin/"))){
-		_cgi_flag = true;
-		return true;
-	}
 	size_t index_virt = _srv->getVirtHostIndex(get("host"));
 	auto value = _srv->config.getBestValues(index_virt, _target, "cgi_ext", {""});
 	if (value[0] == "")
